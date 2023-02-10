023df585e6f0df01423727dfe1dd5516b4980f3a
Alessandro Zanin <alezanin21@gmail.com>
2023-02-10 16:08:04 +0100

    Initial Commit

---
+++ Biancolatte/BiancolatteApp.swift
@@ -0,0 +1,17 @@
+//
+//  BiancolatteApp.swift
+//  Biancolatte
+//
+//  Created by Alessandro Zanin on 10/02/23.
+//
+
+import SwiftUI
+
+@main
+struct BiancolatteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
