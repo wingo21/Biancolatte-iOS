9e839abc2dff134f65b74394828ab35bf2806c53
Alessandro Zanin <alezanin21@gmail.com>
2023-02-17 15:54:12 +0100

    Fixed glitched map icon, but now a shower of warnings appears whenever the map is in use (Xcode glitch)

--- Biancolatte/Views/MapsView.swift
+++ Biancolatte/Views/MapsView.swift
@@ -16,15 +16,11 @@ struct BLLocation: Identifiable {
     var coordinate: CLLocationCoordinate2D
 }
 
-// This extention defines the list of locations to mark up on the map, adding an object on this list will add another location on the map
-extension BLLocation {
-    static var POI: [BLLocation] {
-        [
-            BLLocation(name: "Williamsburg Location", coordinate: CLLocationCoordinate2D(latitude: 40.72023577087259, longitude: -73.95517017329159)),
-            BLLocation(name: "Hudson Yards Location", coordinate: CLLocationCoordinate2D(latitude: 40.75349031717275, longitude: -74.00100907329055))
-        ]
-    }
-}
+// This is the list of Locations, to add a new one create a new BLLocation object, specifiying name and coordinates (grab from GMaps)
+let PointsOfInterest = [
+        BLLocation(name: "Williamsburg", coordinate: CLLocationCoordinate2D(latitude: 40.72023577087259, longitude: -73.95517017329159)),
+        BLLocation(name: "Hudson Yards", coordinate: CLLocationCoordinate2D(latitude: 40.75349031717275, longitude: -74.00100907329055))
+]
 
 struct MapsView: View {
     
@@ -33,20 +29,25 @@ struct MapsView: View {
     @State private var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 40.74289139001299, longitude: -73.97300144703335), latitudinalMeters: 7500, longitudinalMeters: 7500)
     
     var body: some View {
+        
         ZStack {
             //Screen color set to PrimaryColor
             Color("PrimaryColor").edgesIgnoringSafeArea(.all)
             VStack {
                 Text("Our Locations").title()
-                Map(coordinateRegion: $region, annotationItems: BLLocation.POI) { item in
+                Map(coordinateRegion: $region, annotationItems: PointsOfInterest) { item in
+                    //TODO: Al momento un bug di Xcode crea una pioggia di warning ogni volta che si fa qualcosa sulla mappa, non sembra che causino crash dell'app o altro. Bisogna aspettare un'aggiornamento come unica soluzione a quanto pare
                     MapAnnotation(coordinate: item.coordinate) {
-                        ZStack {
+                        VStack {
                             // TODO: Icona Biancolatte rimane troppo piccola
                             Image("MapIcon")
+                            Text(item.name).fixedSize().font(.footnote)
                         }
                     }
                 }
-            }.edgesIgnoringSafeArea(.bottom)
+            }
+            .edgesIgnoringSafeArea(.bottom)
+            
         }
     }
 }
